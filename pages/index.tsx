d27835c5197779771f8dfa92e43c9662491cf4c9
veepgoose <victoriaplatt@loveplanes.co.uk>
2024-01-20 17:25:16 +0000

    Fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Page() {
   
   return <div className='flex flex-col items-center'>
-    <Image src="/assets/FelizHikeLogo.png" width={300} height={300}/>
+    <Image src="/assets/FelizHikeLogo.png" width={300} height={300} alt={""}/>
   <LoginForm/>
   </div>
 }
