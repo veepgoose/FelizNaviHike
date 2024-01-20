3f93a41e327b565a77feb4868c7a891df43b1700
James Vickery <dev@jmsv.me>
2024-01-20 15:09:09 +0000

    fix walks images data

--- data/walks.ts
+++ data/walks.ts
@@ -4,7 +4,7 @@ export const walks =
     "id": "02d5b35f-ff5b-4a8b-93d7-deabed961e63",
     "userId": "cf4a3920-7104-4c22-ac30-95b827a7d01f",
     "name": "Boxing Day Walkies",
-    "imageUrl": "./assets/WalkImg1.jpg",
+    "imageUrl": "/assets/WalkImg1.jpg",
     "descriptionHtml": "<p>A nice chill boxing day walk!</p><p>We'll be getting started at 11AM at Boldmere Gate Car Park. Dogs are not only welcome, but heartily encouraged.</p>",
     "location": {
       "name": "Sutton Park",
@@ -17,7 +17,7 @@ export const walks =
     "id": "beb825fa-82d8-4f73-999e-556c253575e6",
     "userId": "8d577c30-b3ac-4e01-b401-7dcb08149504",
     "name": "Christmas Afternoon Stroll",
-    "imageUrl": "./assets/WalkImg2.jpg",
+    "imageUrl": "/assets/WalkImg2.jpg",
     "descriptionHtml": "<p>Let's get together for a nice afternoon stroll on Christmas Day!</p><p>We'll be getting started at 3PM at Lickey Hills Visitor Centre.</p>",
     "location": {
       "name": "Lickey Hills",
@@ -31,6 +31,7 @@ export const walks =
     "id": "beb825fa-82d8-4f73-999e-556c267575e6",
     "userId": "8d577c30-b3ac-4g01-b401-7dcb08149504",
     "name": "Christmas Pub Crawl paraded as a Walk",
+    "imageUrl": "/assets/WalkImg2.jpg",
     "descriptionHtml": "<p>Let's get together for a nice afternoon stroll (Drink!) on Christmas Day!</p><p>We'll be getting started at 1PM at The Juke in Kings Heath.</p>",
     "location": {
       "name": "Kings Heath/Moseley",
@@ -44,7 +45,7 @@ export const walks =
     "id": "beb825fa-82h8-4f73-999e-556c253575e6",
     "userId": "8d577c30-b3ac-4e01-b401-7dcb08449504",
     "name": "Festive Morning Walk",
-    "imageUrl": "./assets/WalkImg1.jpg",
+    "imageUrl": "/assets/WalkImg1.jpg",
     "descriptionHtml": "<p>Walk and talk with new festive faces!</p><p>We'll be getting started at 10AM at Canon Hill Park, Edgbaston.</p>",
     "location": {
       "name": "Canon Hill Park",
@@ -58,7 +59,7 @@ export const walks =
     "id": "beb815fa-82d8-4f73-999e-556c253575e6",
     "userId": "8d577c30-b3ac-4e01-b201-7dcb08149504",
     "name": "Christmas Afternoon Stroll",
-    "imageUrl": "./assets/WalkImg1.jpg",
+    "imageUrl": "/assets/WalkImg1.jpg",
     "descriptionHtml": "<p>Let's get together for a nice afternoon stroll on Christmas Day!</p><p>We'll be getting started at 3PM at Lickey Hills Visitor Centre.</p>",
     "location": {
       "name": "Lickey Hills",
@@ -72,7 +73,7 @@ export const walks =
     "id": "beb815ga-82d8-4f73-999e-556c253575e6",
     "userId": "8d577c30-b3ac-1e01-b201-7dcb08149504",
     "name": "Late Night Christmas Walk",
-    "imageUrl": "./assets/WalkImg1.jpg",
+    "imageUrl": "/assets/WalkImg1.jpg",
     "descriptionHtml": "<p>Let's get together for a moonlit stroll on Christmas Eve!</p><p>We'll be getting started at 8PM at St Paul's Cathedral.</p>",
     "location": {
       "name": "St Paul's Cathedral",
