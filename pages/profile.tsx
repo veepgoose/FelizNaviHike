239c487d20febbdeeb966ca65c964a492380a5ad
Quinnlizzy <quin27@hotmail.co.uk>
2024-01-20 11:26:19 +0000

    pages setup

---
+++ pages/profile.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>Hello, this is the profile page!</h1>
+  }
---
+++ pages/walkpage.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+    return <h1>Hello, this is where we will display your chosen walk</h1>
+  }
